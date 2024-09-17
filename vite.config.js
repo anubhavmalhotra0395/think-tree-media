c2e537113277d1c60ddd9a575a6a4853c32cd109
anubhavmalhotra0395 <malhotraanubhav0395@gmail.com>
2024-09-17 11:42:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base:"/think-tree-media",
   plugins: [react()],
